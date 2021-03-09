b878bd38eea64b0083451cf9963ec8f20ec1a579
cheikhabh <cheikh_abdallah@yahoo.fr>
2021-03-09 18:16:56 +0100

    Ajoutez des fichiers projet.

---
+++ MVC/Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public async Task<IActionResult> Index(string searchString)
+{
+    var movies = from m in _context.Movie
+                 select m;
+
+    if (!String.IsNullOrEmpty(searchString))
+    {
+        movies = movies.Where(s => s.Title.Contains(searchString));
+    }
+
+    return View(await movies.ToListAsync());
+}
